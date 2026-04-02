157d4d14a198cde9b93452912b845a57d174aa7c
Michael John Samonte <mjsamonte.work@gmail.com>
2026-04-02 18:26:29 +0800

    Rename app from "DC Controller" to "Easy Remote" across configuration files, update app name in strings.xml and index.html, and replace associated icon assets.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.dccontroller.app',
-  appName: 'DC Controller',
+  appName: 'Easy Remote',
   webDir: 'www'
 };
 
